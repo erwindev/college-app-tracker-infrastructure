1ef924d888a8c9f9f480490ba40fbe6a0cb1ea04
Erwin Alberto <erwinalberto@Erwins-MacBook-Pro.local>
2017-08-13 20:44:15 -0400

    refactored ansible playbooks and installed docker

--- infrastructure/dev/outputs.tf
+++ infrastructure/dev/outputs.tf
@@ -1,3 +1,11 @@
 output "elb_hostname" {
   value = "${module.web.elb.hostname}"
 }
+
+output "private_instance_ips" {
+	value = "${module.web.private.instance.ips}"
+}
+
+output "bastion_instance_ip" {
+	value = "${module.vpc.bastion.ip}"
+}
